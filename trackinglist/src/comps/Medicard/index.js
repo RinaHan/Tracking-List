b7b6efe48f1ab15cc1e33b773207dd342355e613
Rina <rinahan4146@gmail.com>
2021-01-27 23:47:50 -0800

    finished Medicard comps

--- trackinglist/src/comps/Medicard/index.js
+++ trackinglist/src/comps/Medicard/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import alarm from "comps/Medicard/alarm.png";
 
 const Container = styled.div`
   display: flex;
 `;
+
 const CardLeft = styled.div`
   width: 75px;
   height: 125px;
@@ -13,97 +15,99 @@ const CardLeft = styled.div`
 
 const Time = styled.div`
   color: #fff;
-  
   display: flex;
-  
   width: 100%;
   height: 100%;
   align-items: center;
   justify-content: center;
-
 `;
 
 const Bold = styled.div`
- font-weight:700;
- font-size:18px;
-  
+  font-weight: 700;
+  font-size: 18px;
 `;
-const HourNum = styled.div`
 
-`;
-const MinNum = styled.div`
- 
-`;
+const HourNum = styled.div``;
+const MinNum = styled.div``;
 
 const NormalFont = styled.div`
-font-size:10px;
-padding-left:9px;
+  font-size: 10px;
+  padding-left: 5px;
 `;
+
 const HR = styled.div`
-padding-bottom:10px;
-
+  padding-bottom: 10px;
 `;
 
-const MIN= styled.div``;
-
+const MIN = styled.div``;
 
 const CardRight = styled.div`
-display:flex;
-flex-direction:column;
-  width: 195px;
+  display: flex;
+  flex-direction: column;
+  width: 200px;
   height: 125px;
   border-radius: 0 25px 25px 0;
   background-color: #fff;
-  justify-content:center;
-  align-items:flex-start;
-  padding-left:20px;
-`;
-const MediName= styled.div`
-font-size: 16px;
-font-weight:700;
-color: #000;
-`;
-const Dosage= styled.div`
-font-size: 12px;
-padding-top:10px;
-/* display:inline-flex; */
-`;
-const ButtomCont= styled.div`
-font-size: 12px;
-display:inline-flex;
-align-items:center;
-justify-content:space-between;
-padding-top:10px;
-width:90%;
-`;
-const Timer= styled.div`
-font-size: 7px;
-/* padding-right:20px; */
-width:62px;
-height:15px;
-background-color:#E6E6E6;
-border-radius:50px;
-`;
-const Edit= styled.button`
-font-size: 12px;
-border:none;
-width:86px;
-height:28px;
-border-radius:10px;
-background-color:#E6E6E6;
+  justify-content: center;
+  align-items: flex-start;
+  padding-left: 15px;
 `;
 
+const MediName = styled.div`
+  font-size: 16px;
+  font-weight: 700;
+  color: #000;
+`;
 
+const Dosage = styled.div`
+  font-size: 12px;
+  padding-top: 10px;
+`;
 
+const ButtomCont = styled.div`
+  font-size: 12px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 20px;
+  width: 90%;
+`;
 
-const Medicard = () => {
+const Timer = styled.div`
+  font-size: 7px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  width: 62px;
+  height: 15px;
+  background-color: #e6e6e6;
+  border-radius: 50px;
+`;
+
+const Edit = styled.button`
+  font-size: 12px;
+  font-weight: 500px;
+  border: none;
+  width: 86px;
+  height: 28px;
+  border-radius: 10px;
+  background-color: #e6e6e6;
+`;
+
+const Icon = styled.img`
+  width: 8px;
+  height: 8px;
+  padding-right: 5px;
+`;
+
+const Medicard = ({ hr, min, mediname, child, mom, timer }) => {
   return (
     <Container>
       <CardLeft>
         <Time>
           <Bold>
-            <HourNum>te</HourNum>
-            <MinNum>St</MinNum>
+            <HourNum>{hr}</HourNum>
+            <MinNum>{min}</MinNum>
           </Bold>
 
           <NormalFont>
@@ -113,17 +117,27 @@ const Medicard = () => {
         </Time>
       </CardLeft>
       <CardRight>
-          <MediName>TestTestTestTest</MediName>
-          <Dosage>1/1</Dosage>
-          <ButtomCont>
-              <Timer>test</Timer>
-              <Edit>Edit</Edit>
-          </ButtomCont>
+        <MediName>{mediname}</MediName>
+        <Dosage>{child}/{mom}</Dosage>
+        <ButtomCont>
+          <Timer>
+            <Icon src={alarm} />
+            {timer} AM
+          </Timer>
+          <Edit>Edit</Edit>
+        </ButtomCont>
       </CardRight>
     </Container>
   );
 };
 
-Medicard.defaultProps = {};
+Medicard.defaultProps = {
+  hr: "00",
+  min: "15",
+  mediname: "Amlodipine Besylate",
+  child:"1",
+  mom:"1",
+  timer: "8:30",
+};
 
 export default Medicard;
