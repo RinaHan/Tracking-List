7cd924b72e89b5aa51eeb9dffd4ad6886fcc8bd9
Farhaz Ladha <farhaz@iMac.hitronhub.home>
2021-02-05 17:59:06 -0800

    Added time picker

--- trackinglist/src/comps/Form/index.js
+++ trackinglist/src/comps/Form/index.js
@@ -1,7 +1,7 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import styled from 'styled-components';
 import { AiOutlinePlus } from 'react-icons/ai';
-import {IoIosArrowUp} from 'react-icons/io';
+import { IoIosArrowUp } from 'react-icons/io';
 
 const Container = styled.div`
 @media (max-width:2000px) and (min-width:200px){
@@ -53,8 +53,8 @@ const FormInput = styled.input`
     padding-left:5px;
 `;
 const InputCont = styled.div`
-    width:${props=>props.width ? props.width : "330px"};
-    height:${props=>props.height ? props.height : "40px"};
+    width:${props => props.width ? props.width : "330px"};
+    height:${props => props.height ? props.height : "40px"};
     margin-top:20px;
 `;
 const FormButton = styled.button`
@@ -62,7 +62,7 @@ const FormButton = styled.button`
     height:36px;
     border-radius:12px;
     border:none;
-    background-color:${props=>props.buttonColor ? props.buttonColor : "#e8e8e8"};
+    background-color:${props => props.buttonColor ? props.buttonColor : "#e8e8e8"};
     margin-top:40px;
     margin-bottom:20px;
     display:flex;
@@ -95,7 +95,7 @@ const Icon2 = styled.div`
     display:${(props) => (props.display3 ? "none" : "flex")};
 `;
 
-const Form = ({buttonText, FormType, onFormComplete, onFormClose}) => {
+const Form = ({ buttonText, FormType, onFormComplete, onFormClose, minuteNum }) => {
 
     const [expand, setExpand] = useState(true);
     const [display1, setDisplay1] = useState(true);
@@ -107,55 +107,61 @@ const Form = ({buttonText, FormType, onFormComplete, onFormClose}) => {
     const [hour, setHour] = useState("");
     const [minute, setMinute] = useState("");
 
-    const handleExpand = () =>{
+    const handleExpand = () => {
         setExpand(!expand);
         setDisplay1(!display1);
         setDisplay2(!display2);
         setDisplay3(!display3);
     }
 
-    return<Container >
-                <Close onClick={handleExpand}>
-                    <Title display1={display1}><h3>Add Medication</h3></Title>
-                    <Icon1 display2={display2}><AiOutlinePlus size={25} /></Icon1>
-                    <Icon2 display3={display3}><IoIosArrowUp size={35} /></Icon2>
-                </Close>
-            <Content expand={expand}>
-                    <WordCont><h4>Add Medication</h4>  </WordCont>
-                    <WordCont ><h4 style={{color: "grey"}}>Medication Name</h4></WordCont>
-                    <InputCont><FormInput type='text' style={{fontSize: "16px"}} onChange={(e)=>{
-                        setMedication(e.target.value);
-                    }}/></InputCont>
-                    <WordCont ><h4 style={{color: "grey"}}>Dosage</h4></WordCont>
-                    <InputCont><FormInput type='number' placeholder="mg"style={{fontSize: "16px"}} onChange={(e)=>{
-                        setDosage(e.target.value);
-                    }}/></InputCont>
-                    <div style={{display:"flex", flexDirection:"row", width:"100%", color:"grey"}}><WordCont><h4>Hour</h4><h4 style={{paddingLeft: "30px"}}>Minute</h4></WordCont></div>
-                    <div style={{width: "100%", display: "flex", flexDirection:"row"}}>   
-                    <InputCont height={"40px"} width={"50px"}><FormInput type="number"style={{fontSize: "20px"}} onChange={(e)=>{
-                        setHour(e.target.value);
-                    }}/></InputCont>
-                    <InputCont style={{paddingLeft: "20px"}} height={"40px"} width={"50px"}><FormInput type="number"style={{fontSize: "20px"}} onChange={(e)=>{
-                        setMinute(e.target.value);
-                    }}/></InputCont>
-                    </div>
-                    <div style={{display:"flex", flexDirection:"row", width:"100%", justifyContent:"space-evenly"}}>
-                    <FormButton onClick={()=>{
-                        onFormClose(handleExpand)
-                    }}><p>Cancel</p></FormButton>
-                    <FormButton buttonColor={"#18A0FB"} onClick={()=>{
-                        onFormComplete(medication, dosage, hour, minute, handleExpand)
-                    }}><p style={{color:"white"}}>{buttonText}</p> </FormButton>
-                    </div>
-            </Content>
+    return <Container >
+        <Close onClick={handleExpand}>
+            <Title display1={display1}><h3>Add Medication</h3></Title>
+            <Icon1 display2={display2}><AiOutlinePlus size={25} /></Icon1>
+            <Icon2 display3={display3}><IoIosArrowUp size={35} /></Icon2>
+        </Close>
+        <Content expand={expand}>
+            <WordCont><h4>Add Medication</h4>  </WordCont>
+            <WordCont ><h4 style={{ color: "grey" }}>Medication Name</h4></WordCont>
+            <InputCont><FormInput type='text' style={{ fontSize: "16px" }} onChange={(e) => {
+                setMedication(e.target.value);
+            }} /></InputCont>
+            <WordCont ><h4 style={{ color: "grey" }}>Dosage</h4></WordCont>
+            <InputCont><FormInput type='number' placeholder="mg" style={{ fontSize: "16px" }} onChange={(e) => {
+                setDosage(e.target.value);
+            }} /></InputCont>
+
+            {/* <div style={{ display: "flex", flexDirection: "row", width: "100%", color: "grey" }}><WordCont><h4>Hour</h4><h4 style={{ paddingLeft: "30px" }}>Minute</h4></WordCont></div> */}
+
+            <WordCont ><h4 style={{ color: "grey" }}>Time</h4></WordCont>
+            <InputCont><FormInput type="time" style={{ fontSize: "20px" }} onChange={(e) => {
+                    setMinute(e.target.value);
+                }} /></InputCont>
+            {/* <div style={{ width: "100%", display: "flex", flexDirection: "row" }}>
+                <InputCont height={"40px"} width={"50px"}><FormInput type="number" min="0" max="24" style={{ fontSize: "20px" }} onChange={(e) => {
+                    setHour(e.target.value);
+                }} /></InputCont>
+                <InputCont style={{ paddingLeft: "20px" }} height={"40px"} width={"50px"}><FormInput type="number" step="5" min="0" max="55" style={{ fontSize: "20px" }} onChange={(e) => {
+                    setMinute(e.target.value);
+                }} /></InputCont>
+            </div> */}
+            <div style={{ display: "flex", flexDirection: "row", width: "100%", justifyContent: "space-evenly" }}>
+                <FormButton onClick={() => {
+                    onFormClose(handleExpand)
+                }}><p>Cancel</p></FormButton>
+                <FormButton buttonColor={"#18A0FB"} onClick={() => {
+                    onFormComplete(medication, dosage, hour, minute, handleExpand)
+                }}><p style={{ color: "white" }}>{buttonText}</p> </FormButton>
+            </div>
+        </Content>
     </Container>
 }
 
 Form.defaultProps = {
-    buttonText:"default",
-    FormType:"default",
-    onFormComplete:()=>{},
-    onFormClose:()=>{},
+    buttonText: "default",
+    FormType: "default",
+    onFormComplete: () => { },
+    onFormClose: () => { },
 
 }
 
